da43c016be2f0510190334b402e3a497953e5255
Topias Mariapori <topias@mariapori.fi>
2021-05-20 01:52:15 +0300

    Paranneltu sposti

--- kirjoitalankaan.php
+++ kirjoitalankaan.php
@@ -17,14 +17,25 @@ $msg = mysqli_escape_string($conn,$_POST["Viesti"]);
 $kirjoittaja = $_SESSION["KayttajaID"];
 $date = date('Y-m-d H:i:s');
 $result = mysqli_query($conn,"INSERT INTO langanviestit (Lanka,Viesti,Kirjoittaja) VALUES ('$lankaid','$msg','$kirjoittaja');");
+$kirjoittajanimisql = mysqli_query($conn, "SELECT Kayttajanimi FROM kayttajat WHERE KayttajaID = '$kirjoittaja'");
+$kirjoittajanimi = "";
+
+$lankanimisql = mysqli_query($conn, "SELECT Nimi FROM langat WHERE LankaID = '$lankaid'");
+$lankanimi = "";
+
+while($rivi = mysqli_fetch_assoc($kirjoittajanimisql)){
+    $kirjoittajanimi = $rivi["Kayttajanimi"];
+}
+while($rivi2 = mysqli_fetch_assoc($lankanimisql)){
+    $lankanimi = $rivi2["Nimi"];
+}
 
 $onkoposti = mysqli_query($conn,"SELECT * FROM langat LEFT JOIN kayttajat k on k.KayttajaID = Luonut WHERE LankaID = '$lankaid'");
-
 if(mysqli_num_rows($onkoposti) > 0){
     while ($row = mysqli_fetch_assoc($onkoposti)){
         if($row["Sposti"] != null && $row["Sposti"] != ""){
             if($kirjoittaja != $row["Luonut"]){
-                mail($row["Sposti"],"Uusi viesti lankaan", $msg);
+                mail($row["Sposti"],"Uusi viesti lankaan ". $lankanimi, $kirjoittajanimi." kirjoitti: ".$msg);
             }
         }
     }
