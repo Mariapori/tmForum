bf0dc72426392a197993f4d303192cb26b10010b
Topias Mariapori <topias@mariapori.fi>
2021-05-20 00:52:48 +0300

    go

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user "root" with no password) */
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "asd");
+define("DB_SERVER", "192.168.64.2");
+define("DB_USERNAME", "root");
 define("DB_PASSWORD", "");
-define("DB_NAME", "tmForum");
+define("DB_NAME", "cpm836475_tmForum");
  
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
