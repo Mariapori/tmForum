8032e31efb24d17bc513266b9d4d894cae185d21
Topias Mariapori <Mariapori@users.noreply.github.com>
2021-08-11 12:52:49 +0300

    Update createdatabase.sql

--- createdatabase.sql
+++ createdatabase.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: 11.05.2021 klo 00:34
--- Palvelimen versio: 10.4.17-MariaDB
--- PHP Version: 8.0.0
+-- Host: localhost:3306
+-- Generation Time: Aug 11, 2021 at 12:49 PM
+-- Server version: 10.3.31-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,28 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `tmforum`
+-- Database: `cpm836475_tmForum`
 --
 
 -- --------------------------------------------------------
 
 --
--- Rakenne taululle `kayttajat`
+-- Table structure for table `kayttajat`
 --
 
 CREATE TABLE `kayttajat` (
   `KayttajaID` int(11) NOT NULL,
   `Kayttajanimi` varchar(255) NOT NULL,
   `Salasana` varchar(255) NOT NULL,
-  `Kuva` varchar(255) NOT NULL,
-  `Rooli` varchar(50) NOT NULL
+  `Kuva` varchar(255) NOT NULL DEFAULT 'https://img.ilcdn.fi/W3NZqtFn2x5avcK1p5wUuTDMS7M=/full-fit-in/612x0/img-s3.ilcdn.fi/4553c0d5b295cd783ea2f7498ea41dcee06fc3212cff54ab2ac292df2497e2a2.jpg',
+  `Rooli` varchar(50) NOT NULL,
+  `Sposti` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- --------------------------------------------------------
-
 --
--- Rakenne taululle `langanviestit`
+-- Table structure for table `langanviestit`
 --
 
 CREATE TABLE `langanviestit` (
@@ -50,25 +50,16 @@ CREATE TABLE `langanviestit` (
   `PVM` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
-
--- --------------------------------------------------------
-
 --
--- Rakenne taululle `langat`
+-- Table structure for table `langat`
 --
 
 CREATE TABLE `langat` (
   `LankaID` int(11) NOT NULL,
   `Nimi` varchar(255) NOT NULL,
   `Luonut` int(11) NOT NULL,
-  `PVM` date NOT NULL
+  `PVM` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `kayttajat`
@@ -99,33 +90,33 @@ ALTER TABLE `langat`
 -- AUTO_INCREMENT for table `kayttajat`
 --
 ALTER TABLE `kayttajat`
-  MODIFY `KayttajaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `KayttajaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `langanviestit`
 --
 ALTER TABLE `langanviestit`
-  MODIFY `ViestiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ViestiID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `langat`
 --
 ALTER TABLE `langat`
-  MODIFY `LankaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `LankaID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Rajoitteet vedostauluille
+-- Constraints for dumped tables
 --
 
 --
--- Rajoitteet taululle `langanviestit`
+-- Constraints for table `langanviestit`
 --
 ALTER TABLE `langanviestit`
   ADD CONSTRAINT `Kirjoittaja` FOREIGN KEY (`Kirjoittaja`) REFERENCES `kayttajat` (`KayttajaID`) ON DELETE CASCADE ON UPDATE NO ACTION,
   ADD CONSTRAINT `Lanka` FOREIGN KEY (`Lanka`) REFERENCES `langat` (`LankaID`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
--- Rajoitteet taululle `langat`
+-- Constraints for table `langat`
 --
 ALTER TABLE `langat`
   ADD CONSTRAINT `Luoja` FOREIGN KEY (`Luonut`) REFERENCES `kayttajat` (`KayttajaID`) ON DELETE CASCADE ON UPDATE NO ACTION;
